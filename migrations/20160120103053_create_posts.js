a133523934fe32f4f8cd4419162c445e5cbd8d63
Patrick Dullano <PWDullano@gmail.com>
2016-01-20 11:15:38 -0700

    first set of tests done

---
+++ migrations/20160120103053_create_posts.js
@@ -0,0 +1,12 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('posts', function (table) {
+    table.increments();
+    table.string('author')
+    table.string('body');
+    table.timestamps();
+  })
+};
+
+exports.down = function(knex, Promise) {
+  knex.schema.dropTable('posts')
+};
